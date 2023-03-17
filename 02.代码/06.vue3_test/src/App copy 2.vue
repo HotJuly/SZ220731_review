b33fb88dde09a22f41810ab6194c1d8c366c0069
HotJuly <949588380@qq.com>
2023-03-17 17:09:03 +0800

    86.ref和reactive的区别

---
+++ 02.代码/06.vue3_test/src/App copy 2.vue
@@ -0,0 +1,82 @@
+<template>
+  <h1>obj.name:{{obj.name}}</h1>
+  <h1>user.name:{{user.name}}</h1>
+  <button @click="changeName">改名操作</button>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  // setup(){
+  //   /*
+  //     setup生命周期
+  //       他是最早执行的一个生命周期函数
+  //       它内部没有this,this指向undefined
+  //       所以在Vue3开发的时候,绝大多数代码都是在setup中写,但是不会使用this
+  //       setup中会书写组合API
+  //   */
+  //   console.log('---------setup----------',this)
+  // },
+  // beforeCreate(){
+  //   console.log('-----------beforeCreate------------',this)
+  // },
+  // created(){
+  //   console.log('-----------created------------',this)
+  // }
+}
+</script>
+
+<script setup>
+import {reactive} from 'vue'
+
+/*
+  reactive的使用
+    reactive函数需要接收一个对象数据类型
+    返回值:一个代理对象
+    此处一共存在两个对象
+      元对象是不具有响应式效果的一个普通对象,但是他身上存有数据
+      代理对象是一个具有响应式效果的对象,但是他身上没有数据
+        只要当前操作经过代理对象,那么本次操作一定具有响应式效果
+
+
+    注意:
+      1.元对象没有响应式效果,千万不要去操作他
+
+*/
+// 这个对象称为元对象,就是真正存有数据的对象
+let obj = {
+  name:"xiaoming"
+}
+
+// 这个对象称为代理对象,他没有数据,
+// 开发者对他做的任何事情,他都会对元对象进行操作
+// 该对象具有响应式效果
+let user = reactive(obj);
+
+// console.log(user,obj)
+
+const changeName = ()=>{
+  // obj.name="laowang";
+  user.name="laowang";
+  console.log(obj)
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02.代码/06.vue3_test/src/App copy.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>count:{{count}}</h1>
+  <button @click="add">加一操作</button>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  // setup(){
+  //   /*
+  //     setup生命周期
+  //       他是最早执行的一个生命周期函数
+  //       它内部没有this,this指向undefined
+  //       所以在Vue3开发的时候,绝大多数代码都是在setup中写,但是不会使用this
+  //       setup中会书写组合API
+  //   */
+  //   console.log('---------setup----------',this)
+  // },
+  // beforeCreate(){
+  //   console.log('-----------beforeCreate------------',this)
+  // },
+  // created(){
+  //   console.log('-----------created------------',this)
+  // }
+}
+</script>
+
+<script setup>
+import {ref} from 'vue'
+// setup标签其实是一个语法糖
+// 当解析.vue文件的时候,webpack会将setup标签中的内容变成setup生命周期钩子函数
+// setup标签中,所有的直系变量都会自动暴露出去,给模版进行使用
+
+/*
+  ref的使用
+    返回值:是一个Ref对象
+      ref对象具有一个value属性,具有响应式效果
+
+    1.可以将基本数据类型的数据传入ref函数中,将其变为响应式数据
+
+    使用方法:
+      1.在template中使用
+        Vue在模版中遇到Ref对象,他会自动读取该对象的value进行展示
+
+      2.在js代码中
+        在写代码的时候,如果想要读取或者修改Ref中的值,必须自己写好.value属性写法
+
+*/
+
+let count = ref(0);
+// console.log(count)
+
+const add = ()=>{
+  count.value++;
+  console.log(count)
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
